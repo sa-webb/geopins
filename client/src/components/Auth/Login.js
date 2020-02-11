3b9205244fc37e61bccb651ebc8c523477cd0455
sa-webb <sawebb30@gmail.com>
2020-02-11 09:43:08 -0600

    with Context

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GraphQLClient } from 'graphql-request'; 
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
 // import Typography from "@material-ui/core/Typography";
+
+import Context from '../../context';
 
 const MeQuery = `
 {
@@ -15,13 +17,15 @@ const MeQuery = `
 }`
 
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context)
   const onSuccess = async googleUser => {
     const idToken = googleUser.getAuthResponse().id_token;
     const client = new GraphQLClient('http://localhost:4000/graphql', {
       headers: { authorization: idToken }
     })
     const data = await client.request(MeQuery)
-    console.log({ data })
+    // console.log({ data });
+    dispatch({ type: "LOGIN_USER", payload: data.me })
   }
   return (
     <GoogleLogin
