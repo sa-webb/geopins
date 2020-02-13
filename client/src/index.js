a32de35bde515b1411fd17db0f633093d7d7bb51
sa-webb <sawebb30@gmail.com>
2020-02-12 22:11:20 -0600

    protected /

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import Context from './context';
 import reducer from './reducer';
+import ProtectedRoute from './ProtectedRoute';
 
 import App from './pages/App';
 import Splash from './pages/Splash';
@@ -13,16 +14,17 @@ import Splash from './pages/Splash';
 const Root = () => {
   const initialState = useContext(Context);
   /**
+   * Using Context with Router.
    * @state Application state after the reducer has run.
    * @dispatch Dispatches actions that will change the state.
    */
   const [state, dispatch] = useReducer(reducer, initialState);
-  console.log({ state });
+
   return (
     <Router>
       <Context.Provider value={{ state, dispatch }}>
         <Switch>
-          <Route exact path='/' component={App} />
+          <ProtectedRoute exact path='/' component={App} />
           <Route path='/login' component={Splash} />
         </Switch>
       </Context.Provider>
