8fdc666e7ebe64cce35cfb1064906568637e0944
sa-webb <sawebb30@gmail.com>
2020-02-12 20:09:03 -0600

    final

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import { GraphQLClient } from 'graphql-request'; 
+import { GraphQLClient } from 'graphql-request';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
 import Context from '../../context';
 
@@ -14,25 +14,46 @@ const MeQuery = `
     email
     picture
   }
-}`
+}`;
 
 const Login = ({ classes }) => {
-  const { dispatch } = useContext(Context)
+  const { dispatch } = useContext(Context);
   const onSuccess = async googleUser => {
+    try {
     const idToken = googleUser.getAuthResponse().id_token;
     const client = new GraphQLClient('http://localhost:4000/graphql', {
       headers: { authorization: idToken }
-    })
-    const data = await client.request(MeQuery)
+    });
+    const data = await client.request(MeQuery);
     // console.log({ data });
-    dispatch({ type: "LOGIN_USER", payload: data.me })
+    dispatch({ type: 'LOGIN_USER', payload: data.me });
+  } catch (err) {
+    onFailure()
   }
+}
+  const onFailure = err => {
+    console.error("Error logging in ", err);
+  }
+
   return (
+    <div className={classes.root}>
+      <Typography
+        component="h1"
+        variant="h3"
+        gutterBottom
+        noWrap
+        style={{ color: "rgb(66, 133, 244)" }}
+      >
+        Welcome
+      </Typography>
     <GoogleLogin
       clientId='1076782752033-2knkv8b9tk47s5ddobsv8rrdekt4v4fq.apps.googleusercontent.com'
       onSuccess={onSuccess}
       isSignedIn={true}
+      onFailure={onFailure}
+      theme="dark"
     />
+    </div>
   );
 };
 
