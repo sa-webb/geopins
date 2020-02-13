195e5160b8cbee4ec28c05d6cd153f201349dc1a
sa-webb <sawebb30@gmail.com>
2020-02-12 22:11:31 -0600

    final

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GoogleLogout } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
-// import ExitToApp from "@material-ui/icons/ExitToApp";
-// import Typography from "@material-ui/core/Typography";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import Typography from "@material-ui/core/Typography";
+import { unstable_useMediaQuery as useMediaQuery } from "@material-ui/core/useMediaQuery";
+
+import Context from "../../context";
 
 const Signout = ({ classes }) => {
-  return <div>Signout</div>;
+  const mobileSize = useMediaQuery("(max-width: 650px)");
+  const { dispatch } = useContext(Context);
+
+  const onSignout = () => {
+    dispatch({ type: "SIGNOUT_USER" });
+    console.log("Signed out user");
+  };
+
+  return (
+    <GoogleLogout
+      onLogoutSuccess={onSignout}
+      render={({ onClick }) => (
+        <span className={classes.root} onClick={onClick}>
+          <Typography
+            style={{ display: mobileSize ? "none" : "block" }}
+            variant="body1"
+            className={classes.buttonText}
+          >
+            Signout
+          </Typography>
+          <ExitToAppIcon className={classes.buttonIcon} />
+        </span>
+      )}
+    />
+  );
 };
 
 const styles = {
