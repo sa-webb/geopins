8828f6d45104cfede48c3d110d9cbf2e7a2e7b77
sa-webb <sawebb30@gmail.com>
2020-02-16 23:10:18 -0600

    v1

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,41 +1,67 @@
-import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import React, { useState } from 'react';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import { withStyles } from '@material-ui/core/styles';
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 35.158199909022834,
+  longitude: -84.87738581406285,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        mapboxApiAccessToken='pk.eyJ1IjoicmVlZGJhcmdlcmNvZGVzIiwiYSI6ImNqczVodXgzczAwM3E0M3MydzI0OHN0ZzEifQ.0qj4u8RW-Rj6An3WFLXKqA'
+        width='100vw'
+        height='calc(100vh - 64px)'
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+        onViewportChange={newViewport => setViewport(newViewport)}
+        {...viewport}
+      >
+        <div className={classes.navigationControl}>
+          <NavigationControl
+            onViewportChange={newViewport => setViewport(newViewport)}
+          />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 const styles = {
   root: {
-    display: "flex"
+    display: 'flex'
   },
   rootMobile: {
-    display: "flex",
-    flexDirection: "column-reverse"
+    display: 'flex',
+    flexDirection: 'column-reverse'
   },
   navigationControl: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
-    margin: "1em"
+    margin: '1em'
   },
   deleteIcon: {
-    color: "red"
+    color: 'red'
   },
   popupImage: {
-    padding: "0.4em",
+    padding: '0.4em',
     height: 200,
     width: 200,
-    objectFit: "cover"
+    objectFit: 'cover'
   },
   popupTab: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "column"
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   }
 };
 
