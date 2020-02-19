f80eab95fc5f686805a01fc1f52fe7997318265a
sa-webb <sawebb30@gmail.com>
2020-02-18 20:51:51 -0600

    v1

--- client/src/components/Pin/CreatePin.js
+++ client/src/components/Pin/CreatePin.js
@@ -1,15 +1,137 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from 'axios';
 import { withStyles } from "@material-ui/core/styles";
-// import TextField from "@material-ui/core/TextField";
-// import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-// import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
-// import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
-// import ClearIcon from "@material-ui/icons/Clear";
-// import SaveIcon from "@material-ui/icons/SaveTwoTone";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import AddAPhotoIcon from "@material-ui/icons/AddAPhotoTwoTone";
+import LandscapeIcon from "@material-ui/icons/LandscapeOutlined";
+import ClearIcon from "@material-ui/icons/Clear";
+import SaveIcon from "@material-ui/icons/SaveTwoTone";
+//import { useMediaQuery } from "@material-ui/core/useMediaQuery";
 
+import Context from '../../context';
+import { CREATE_PIN_MUTATION } from '../../graphql/mutations';
+import { useClient } from "../../client";
+
+/**
+ * 
+ * @param {submitting} boolean sets state so multiple submits cannot take place simultaneously. 
+ */
 const CreatePin = ({ classes }) => {
-  return <div>CreatePin</div>;
+  const { state, dispatch } = useContext(Context);
+  const client = useClient();
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [content, setContent] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  //const mobileSize = useMediaQuery("(max-width: 650px)");
+
+  const handleDeleteDraft = () => {
+    setTitle("");
+    setImage("");
+    setContent("");
+    dispatch({ type: "DELETE_DRAFT" });
+  };
+  
+  const handleSubmit = async event => {
+    try {
+      event.preventDefault();
+      setSubmitting(true);
+      const url = await handleImageUpload();
+      const { latitude, longitude } = state.draft;
+      const variables = { title, image: url, content, latitude, longitude };
+      await client.request(CREATE_PIN_MUTATION, variables);
+      handleDeleteDraft(); // nullify draft upon submission
+    } catch (err) {
+      setSubmitting(false);
+      console.error("Error creating pin", err);
+    }
+  };
+
+  const handleImageUpload = async () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "geopins");
+    data.append("cloud_name", "sa-webb");
+    const res = await axios.post(
+      "https://api.cloudinary.com/v1_1/sa-webb/image/upload",
+      data
+    );
+    return res.data.url;
+  };
+
+  return (
+    <form className={classes.form}>
+      <Typography
+        className={classes.alignCenter}
+        component="h2"
+        variant="h4"
+        color="secondary"
+      >
+        <LandscapeIcon className={classes.iconLarge} /> Pin Location
+      </Typography>
+      <div>
+        <TextField
+          name="title"
+          label="Title"
+          placeholder="Insert pin title"
+          onChange={e => setTitle(e.target.value)}
+        />
+        <input
+          accept="image/*"
+          id="image"
+          type="file"
+          className={classes.input}
+          onChange={e => setImage(e.target.files[0])}
+        />
+        <label htmlFor="image">
+          <Button
+            style={{ color: image && "green" }} // if populated, icon turns green
+            component="span"
+            size="small"
+            className={classes.button}
+          >
+            <AddAPhotoIcon />
+          </Button>
+        </label>
+      </div>
+      <div className={classes.contentField}>
+        <TextField
+          name="content"
+          label="Content"
+          multiline
+          //rows={mobileSize ? "3" : "6"}
+          margin="normal"
+          fullWidth
+          variant="outlined"
+          onChange={e => setContent(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button
+          onClick={handleDeleteDraft}
+          className={classes.button}
+          variant="contained"
+          color="primary"
+        >
+          <ClearIcon className={classes.leftIcon} />
+          Discard
+        </Button>
+        <Button
+          type="submit"
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          disabled={!title.trim() || !content.trim() || !image || submitting} // trims blank input, doesn't allow another submit while submitting is true.
+          onClick={handleSubmit}
+        >
+          Submit
+          <SaveIcon className={classes.rightIcon} />
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 const styles = theme => ({
