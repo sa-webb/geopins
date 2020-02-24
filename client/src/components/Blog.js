c1d66d2d83f56377acf3ce4d1226e905eb986a35
sa-webb <sawebb30@gmail.com>
2020-02-24 09:30:16 -0600

    blogContent

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -5,18 +5,22 @@ import Paper from "@material-ui/core/Paper";
 import Context from '../context';
 import NoContent from './Pin/NoContent';
 import CreatePin from './Pin/CreatePin';
+import PinContent from './Pin/PinContent';
 
 const Blog = ({ classes }) => {
   const { state } = useContext(Context);
-  const { draft } = state;
+  const { draft, currentPin } = state;
 
   let BlogContent;
 
-  if (!draft) {
+  if (!draft && !currentPin) {
     BlogContent = NoContent;
-  } else if (draft) {
+  } else if (draft && !currentPin) {
     BlogContent = CreatePin;
+  } else if (!draft && currentPin) {
+    BlogContent = PinContent;
   }
+  
   return (
     <Paper className={classes.root}>
       <BlogContent />
