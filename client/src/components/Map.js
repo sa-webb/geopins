669bb82480c63af98389ac183538e2243fb74c2e
sa-webb <sawebb30@gmail.com>
2020-02-24 11:42:23 -0600

    pinsListener

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -43,13 +43,13 @@ const Map = ({ classes }) => {
 
   const [popup, setPopup] = useState(null);
 
-  // useEffect(() => {
-  //   const pinExists =
-  //     popup && state.pins.findIndex(pin => pin._id === popup._id) > -1;
-  //   if (!pinExists) {
-  //     setPopup(null);
-  //   }
-  // }, [state.pins.length]);
+  useEffect(() => {
+    const pinExists =
+      popup && state.pins.findIndex(pin => pin._id === popup._id) > -1;
+    if (!pinExists) {
+      setPopup(null);
+    }
+  }, [state.pins.length]); // Listen if length of array changes
 
   const getUserPosition = () => {
     if ('geolocation' in navigator) {
