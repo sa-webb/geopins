0bbdc53372b62ca88374d53c52af828d7f841716
sa-webb <sawebb30@gmail.com>
2020-02-24 10:42:09 -0600

    final

--- client/src/components/Comment/CreateComment.js
+++ client/src/components/Comment/CreateComment.js
@@ -1,31 +1,36 @@
-import React, { useState, useContext } from "react";
-import { withStyles } from "@material-ui/core";
-import InputBase from "@material-ui/core/InputBase";
-import IconButton from "@material-ui/core/IconButton";
-import ClearIcon from "@material-ui/icons/Clear";
-import SendIcon from "@material-ui/icons/Send";
-import Divider from "@material-ui/core/Divider";
+import React, { useState, useContext } from 'react';
+import { withStyles } from '@material-ui/core';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import ClearIcon from '@material-ui/icons/Clear';
+import SendIcon from '@material-ui/icons/Send';
+import Divider from '@material-ui/core/Divider';
 
-import { CREATE_COMMENT_MUTATION } from "../../graphql/mutations";
-import { useClient } from "../../client";
-import Context from "../../context";
+import { CREATE_COMMENT_MUTATION } from '../../graphql/mutations';
+import { useClient } from '../../client';
+import Context from '../../context';
+import { CREATE_COMMENT } from '../../actions';
 
 const CreateComment = ({ classes }) => {
   const client = useClient();
-  const { state } = useContext(Context);
-  const [comment, setComment] = useState("");
+  const { state, dispatch } = useContext(Context);
+  const [comment, setComment] = useState('');
 
   const handleSubmitComment = async () => {
     const variables = { pinId: state.currentPin._id, text: comment };
-    await client.request(CREATE_COMMENT_MUTATION, variables);
-    setComment("");
+    const { createComment } = await client.request(
+      CREATE_COMMENT_MUTATION,
+      variables
+    );
+    dispatch({ type: CREATE_COMMENT, payload: createComment})
+    setComment('');
   };
 
   return (
     <>
       <form className={classes.form}>
         <IconButton
-          onClick={() => setComment("")}
+          onClick={() => setComment('')}
           disabled={!comment.trim()}
           className={classes.clearButton}
         >
@@ -33,7 +38,7 @@ const CreateComment = ({ classes }) => {
         </IconButton>
         <InputBase
           className={classes.input}
-          placeholder="Add Comment"
+          placeholder='Add Comment'
           multiline={true}
           value={comment}
           onChange={e => setComment(e.target.value)}
@@ -53,8 +58,8 @@ const CreateComment = ({ classes }) => {
 
 const styles = theme => ({
   form: {
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center'
   },
   input: {
     marginLeft: 8,
@@ -62,7 +67,7 @@ const styles = theme => ({
   },
   clearButton: {
     padding: 0,
-    color: "red"
+    color: 'red'
   },
   sendButton: {
     padding: 0,
